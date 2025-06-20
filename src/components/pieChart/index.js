d5403a550cd50b984cdc63376c15a2c28f38923e
DaveedGangi <daveedgangi@gmail.com>
2025-06-20 12:30:18 +0530

    added description

--- src/components/pieChart/index.js
+++ src/components/pieChart/index.js
@@ -1,6 +1,9 @@
+import "./index.css";
 import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
 
 const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
+
 
 const TransactionPieChart = ({ transactions }) => {
   const categoryData = transactions.reduce((acc, tx) => {
@@ -14,7 +17,10 @@ const TransactionPieChart = ({ transactions }) => {
   }, []);
 
   return (
-    <PieChart width={400} height={300}>
+
+    <div>
+      <div className="big-screen">
+         <PieChart width={500} height={350}>
       <Pie
         data={categoryData}
         cx="50%"
@@ -31,6 +37,28 @@ const TransactionPieChart = ({ transactions }) => {
       <Tooltip />
       <Legend />
     </PieChart>
+      </div>
+      <div className="small-screen">
+           <PieChart width={330} height={330}>
+      <Pie
+        data={categoryData}
+        cx="50%"
+        cy="50%"
+        outerRadius={100}
+        fill="#8884d8"
+        dataKey="value"
+        label
+      >
+        {categoryData.map((_, index) => (
+          <Cell key={index} fill={COLORS[index % COLORS.length]} />
+        ))}
+      </Pie>
+      <Tooltip />
+      <Legend />
+    </PieChart>
+      </div>
+    </div>
+    
   );
 };
 
