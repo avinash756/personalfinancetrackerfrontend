fbaa704aac62ccb7c5e653f613d6a11296c92950
Avinash <avinashgangarapu122@gmail.com>
2025-06-27 11:13:40 +0530

    footerChanged

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,23 +3,36 @@ import "./index.css";
 import { TbBrandLinkedin } from "react-icons/tb";
 import { RiTwitterXFill } from "react-icons/ri";
 import { PiGithubLogoLight } from "react-icons/pi";
-function Footer(){
+function Footer() {
+  return (
+    <div className="footer-section">
+      <p className="footer-para">happy diwali</p>
 
-
-    return(
-
-        <div className="footer-section">
-
-            <p className="footer-para">© 2025 Daveed Gangi. All Rights Reserved.</p>
-
-            <div className="footer-links">
-                <a href="https://www.linkedin.com/in/g-daveed-365958190/" target="_blank" rel="noreferrer">LinkedIn &nbsp; <TbBrandLinkedin /></a>&nbsp;&nbsp;
-                <a href="https://x.com/Daveed53460412" target="_blank" rel="noreferrer">Twitter &nbsp;<RiTwitterXFill /></a>&nbsp;&nbsp;
-                <a href="https://github.com/DaveedGangi" target="_blank" rel="noreferrer">GitHub &nbsp;<PiGithubLogoLight /></a>
-            </div>
-            
-        </div>
-    )
+      <div className="footer-links">
+        <a
+          href="https://www.linkedin.com/in/g-daveed-365958190/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          LinkedIn &nbsp; <TbBrandLinkedin />
+        </a>
+        &nbsp;&nbsp;
+        <a href="https://x.com/Daveed53460412" target="_blank" rel="noreferrer">
+          Twitter &nbsp;
+          <RiTwitterXFill />
+        </a>
+        &nbsp;&nbsp;
+        <a
+          href="https://github.com/DaveedGangi"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub &nbsp;
+          <PiGithubLogoLight />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
